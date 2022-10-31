89310bde080f543f48a57939e5e2fa02c324cd16
Dinakara Sai Santosh Burugupalli <53957775+dburugupalli@users.noreply.github.com>
2022-10-30 19:14:40 -0700

    Create search_router_test.go

---
+++ pkg/app/search_router_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/Cisco-Kosha/twitter-connector/pkg/app"
+	"github.com/Cisco-Kosha/twitter-connector/pkg/models"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetRequest(t *testing.T) {
+	t.Parallel()
+
+	r, _ := http.NewRequest("GET", "/api/v2/agents", nil)
+	w := httptest.NewRecorder()
+
+	a := app.App{}
+	a.ListConnectorSpecification(w, r)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}

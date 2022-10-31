7b756d3a3a279befb922a480b89cd14be6efb3a8
Dinakara Sai Santosh Burugupalli <53957775+dburugupalli@users.noreply.github.com>
2022-10-30 19:38:13 -0700

    Update search_router_test.go

--- pkg/app/search_router_test.go
+++ pkg/app/search_router_test.go
@@ -1,24 +1,18 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/Cisco-Kosha/freshservice-connector/pkg/app"
-	"github.com/Cisco-Kosha/freshservice-connector/pkg/models"
+	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestGetRequest(t *testing.T) {
 	t.Parallel()
-
 	r, _ := http.NewRequest("GET", "/api/v2/agents", nil)
 	w := httptest.NewRecorder()
-
 	a := app.App{}
-	a.ListConnectorSpecification(w, r)
-
+	a.searchTickets(w,r)
 	assert.Equal(t, http.StatusOK, w.Code)
 }
